8212b28acc66533c3c0aea457f7974ce5761aaac
植松有希 <y002uematsu@gmail.com>
2019-11-23 19:48:32 +0900

    PinBallプロジェクト
    通常課題と発展課題のファイルです。

---
+++ Assets/Lesson5/FripperController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+	private HingeJoint myHingeJoint;
+	private float defaultAngle = 20;
+	private float flickAngle = -20;
+
+	private Touch myTouch;
+	private float myScreenWid = 1118;
+
+	// Use this for initialization
+	void Start () {
+		this.myHingeJoint = GetComponent<HingeJoint> ();
+		SetAngle (this.defaultAngle);
+
+		myScreenWid = Screen.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+
+		for(int i = 0;i < Input.touchCount;i++){
+			this.myTouch = Input.GetTouch (i);
+			Debug.Log (myTouch.position);
+
+			if (myTouch.phase == TouchPhase.Began && myTouch.position.x < this.myScreenWid/2 && tag == "LeftFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			if (myTouch.phase == TouchPhase.Ended && myTouch.position.x < this.myScreenWid/2 &&tag == "LeftFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+			if (myTouch.phase == TouchPhase.Began && myTouch.position.x> this.myScreenWid/2 && tag == "RightFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			if (myTouch.phase == TouchPhase.Ended && myTouch.position.x> this.myScreenWid/2 &&tag == "RightFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+		}
+	}
+
+	public void SetAngle(float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
---
+++ Assets/Lesson5/ScoreDisplay.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	private GameObject scoreText;
+	private int score;
+
+
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find ("ScoreText");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+
+		string othertag;
+		othertag = other.gameObject.tag;
+		if (othertag == "SmallStarTag") {
+			this.score += 5;
+		} else if (othertag == "LargeStarTag") {
+			this.score += 10;
+		} else if (othertag == "SmallCloudTag") {
+			this.score += 15;
+		} else if (othertag == "LargeCloudTag") {
+			this.score += 30;
+		}
+		this.scoreText.GetComponent<Text> ().text = "Score:" + this.score;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
